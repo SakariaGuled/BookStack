ef3484c052b0ee8ea2a24ad0732f9bd380467e65
SakariaGuled <sakaria.guled@gmail.com>
2026-06-11 23:22:01 +0200

    lagde skjelletet til prosjektet

---
+++ config/db.js
@@ -0,0 +1,28 @@
+import { PrismaClient } from "../src/generated/prisma/client.ts";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({
+  adapter,
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "error", "warn"]
+      : ["error"],
+});
+
+const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log("DB Connected via prisma");
+  } catch (error) {
+    console.log(`Connection not succesful => error: ${error}`);
+    process.exit(1);
+  }
+};
+
+const disconnetDB = async () => {
+  await prisma.$disconnect();
+};
+
+export { prisma, connectDB, disconnetDB };
---
+++ src/controller/authController.js
@@ -0,0 +1,5 @@
+const register = async (req, res) => {
+  res.json({hello:"my guy"});
+};
+
+export { register };
